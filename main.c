39b046e4b2030251256aa7dbf3059c21686475c0
ivan-dunko <54901330+ivan-dunko@users.noreply.github.com>
2020-10-25 18:06:47 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdint.h>
 
 #ifndef ITER_CNT_CHECK
 #define ITER_CNT_CHECK (size_t)(1e3)
@@ -13,6 +14,15 @@
 #define ERROR_CODE -1
 #define SUCCESS_CODE 0
 #define MIN_ARG_CNT 2
+#define MAX_THREAD_CNT 5000
+
+/*
+    The maximum number of significant iterations:
+        size_t overflow or double underflow
+        are above this limit
+*/
+const size_t iter_cnt_limit = 
+                sizeof(size_t) * 8 >= 53 ? ((size_t)1 << 53) : (size_t)(-1);
 
 void exitWithFailure(const char *msg, int errcode){
     errno = errcode;
@@ -21,8 +31,20 @@ void exitWithFailure(const char *msg, int errcode){
 }
 
 char is_intr = 0;
-char has_no_intr = 0;
-size_t *iter_cnt_arr = NULL;
+
+/*
+    This flag is introduced to not to
+    let happen situation where one thread
+    pass by `if (is_intr)` block at `routine`
+    and another one, after signal`s catched
+    and `is_intr` is set to 1, pass in that block,
+        thus resulting to deadlock.
+
+    i. e. this flag implements
+    sharing same value of `is_intr` flag
+    among threads.
+*/
+char is_intr_detected = 0;
 
 void sigcatch(){
     is_intr = 1;
@@ -38,8 +60,13 @@ typedef struct Context{
             thread_id;
     double *res;
     pthread_barrier_t *barrier;
+    int return_code;
 } Context;
 
+/*
+    Returns ind`th addendum
+    of sequence
+*/
 double addendum(size_t ind){
     double denum = (ind) * 2.0 + 1.0;
         if (ind % 2 == 1)
@@ -48,13 +75,16 @@ double addendum(size_t ind){
     return 1.0 / denum;
 }
 
-size_t maxIterCntArray(size_t thread_cnt){
-    size_t max_iter_cnt = 0;
-    for (int i = 0; i < thread_cnt; ++i)
-        if (iter_cnt_arr[i] > max_iter_cnt)
-            max_iter_cnt = iter_cnt_arr[i];
-
-    return max_iter_cnt;
+/*
+    Checks `pthread_barrier_wait` return code:
+        in case it`s not successful
+        this function stops current thread
+*/
+void assertBarrierWaitSuccess(Context *cntx, int err){
+    if (err != SUCCESS_CODE && err != PTHREAD_BARRIER_SERIAL_THREAD){
+        cntx->return_code = err;
+        pthread_exit((void*)ERROR_CODE);
+    }
 }
 
 void *routine(void *data){
@@ -70,40 +100,44 @@ void *routine(void *data){
     size_t iter_cnt = 0;
     size_t iter_cnt_check = 0;
     while(1){
+        if (iter_cnt == iter_cnt_limit){
+            *(cntx->res) = sum;
+            pthread_exit((void*)SUCCESS_CODE);
+        }
+
         if (iter_cnt_check == ITER_CNT_CHECK){
-            if (!is_intr)
-                has_no_intr = 1;
+            if (is_intr)
+                is_intr_detected = 1;
+            /*
+                this `pthread_barrier_wait` placed here to
+                synchronize threads on number of made
+                iterations ans properly set `is_intr_detected`
+            */
             int err = pthread_barrier_wait(cntx->barrier);
-            if (err != SUCCESS_CODE && err != PTHREAD_BARRIER_SERIAL_THREAD)
-                return err;
+            assertBarrierWaitSuccess(cntx, err);
 
-            if (!has_no_intr){
-                iter_cnt_arr[cntx->thread_id] = iter_cnt;
-                err = pthread_barrier_wait(cntx->barrier);
-                if (err != SUCCESS_CODE && err != PTHREAD_BARRIER_SERIAL_THREAD)
-                    return err;
-
-                size_t max_iter_cnt = maxIterCntArray(cntx->thread_cnt);
-
-                while (iter_cnt < max_iter_cnt){
-                    sum += addendum(ind);
-                    ind += cntx->thread_cnt;
-                    ++iter_cnt;
-                }
-
+            if (is_intr_detected){
                 *(cntx->res) = sum;
                 pthread_exit((void*)SUCCESS_CODE);
             }
 
             iter_cnt_check = 0;
+            /*
+                this `pthread_barrier_wait` placed here to
+                not to let some thread set `is_intr_detected`
+                to 1 if was set to 0. Consider example:
 
+                1) is_intr_detected = 0 at upper barrier (A)
+                2) first thread continues execution and makes
+                    ITER_CNT_CHECK iterations
+                3) `is_intr` is set to 1 and thus first thread
+                    sets `is_intr_detected` to 1 and blocks
+                4) second thread continues execution after (A)
+                    and `is_intr_detected` is set to 1:
+                        second threads stops execution -> deadlock
+            */
             err = pthread_barrier_wait(cntx->barrier);
-            if (err != SUCCESS_CODE && err != PTHREAD_BARRIER_SERIAL_THREAD)
-                    return err;
-            has_no_intr = 0;
-            err = pthread_barrier_wait(cntx->barrier);
-            if (err != SUCCESS_CODE && err != PTHREAD_BARRIER_SERIAL_THREAD)
-                    return err;
+            assertBarrierWaitSuccess(cntx, err);
         }
 
         sum += addendum(ind);
@@ -130,12 +164,11 @@ int init(
     *pid = (pthread_t*)malloc(sizeof(pthread_t) * thread_cnt);
     if (*pid == NULL)
         return ENOMEM;
+
     *cntx = (Context*)malloc(sizeof(Context) * thread_cnt);
     if (cntx == NULL)
         return ENOMEM;
-    iter_cnt_arr = (size_t*)malloc(sizeof(size_t) * thread_cnt);
-    if (iter_cnt_arr == NULL)
-        return ENOMEM;
+
     int err = pthread_barrier_init(barrier, NULL, thread_cnt);
     if (err != SUCCESS_CODE)
         return err;
@@ -144,6 +177,7 @@ int init(
         (*(cntx) + i)->thread_cnt = thread_cnt;
         (*(cntx) + i)->thread_id = i;
         (*(cntx) + i)->barrier = barrier;
+        (*(cntx) + i)->return_code = SUCCESS_CODE;
         (*(cntx) + i)->res = (double*)malloc(sizeof(double));
         if ((*(cntx) + i)->res == NULL)
             return ENOMEM;
@@ -156,7 +190,7 @@ int init(
     return SUCCESS_CODE;
 }
 
-double gatherPartialSums(
+int gatherPartialSums(
     pthread_t *pid, 
     Context *cntx, 
     size_t thread_cnt,
@@ -171,7 +205,15 @@ double gatherPartialSums(
         int err = pthread_join(pid[i], NULL);
         if (err != SUCCESS_CODE)
             return err;
+        if (cntx->return_code != SUCCESS_CODE)
+            return cntx->return_code;
 
+        /*
+            despite floating point arithmetics
+            is not associative, since order of summation
+            is the same all the time, result always will be
+            the same for fixed number of iterations
+        */
         sum += *(cntx[i].res);
     }
     
@@ -185,7 +227,7 @@ int releaseResources(
     pthread_barrier_t *barrier){
     
     if (pid == NULL || cntx == NULL || 
-        iter_cnt_arr == NULL || barrier == NULL)
+        barrier == NULL)
         return EINVAL;
 
     size_t thread_cnt = cntx[0].thread_cnt;
@@ -194,7 +236,6 @@ int releaseResources(
 
     free(pid);
     free(cntx);
-    free(iter_cnt_arr);
     int err = pthread_barrier_destroy(barrier);
     if (err != SUCCESS_CODE)
         return err;
@@ -205,11 +246,16 @@ int releaseResources(
 int main(int argc, char **argv){
     char *endptr;
     errno = SUCCESS_CODE;
+
     const size_t thread_cnt = 
         argc >= MIN_ARG_CNT ? strtoll(argv[1], &endptr, 10) : 4;
-    if (errno != SUCCESS_CODE || strtoll(argv[1], &endptr, 10) < 0)
+    if (argc >= MIN_ARG_CNT 
+        && (errno != SUCCESS_CODE || strtoll(argv[1], &endptr, 10) < 0))
         exitWithFailure("main", EINVAL);
-
+        
+    if (thread_cnt > MAX_THREAD_CNT)
+        exitWithFailure("main: too much threads", EINVAL);
+    
     pthread_t *pid;
     Context *cntx;
     pthread_barrier_t barrier;
